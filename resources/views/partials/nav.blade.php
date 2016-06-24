fd37a0fbdc81af3b0cdd9e576430ad98be03c7e4
sebdsz <sebdesquirez@gmail.com>
2016-06-24 16:27:22 +0200

    update

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <ul class="nav navbar-nav">
             <li><a href="{{ action('FrontController@home') }}">Bananier</a></li>
-            <li><a href="{{ action('FrontController@units') }}">Maison des primates</a></li>
-            <li><a href="{{ action('FrontController@recruits') }}">Singeries</a></li>
-            <li><a href="#">Construction</a></li>
-            <li><a href="#">Maison du primate savant</a></li>
-            <li><a href="{{ action('FrontController@resources') }}">Ressources</a></li>
+            <li><a href="{{ action('FrontController@units') }}">Mon armée</a></li>
+            <li><a href="{{ action('FrontController@recruits') }}">Recrutements</a></li>
+            <li><a href="#">Constructions</a></li>
+            <li><a href="#">Technologies</a></li>
+            <li><a href="{{ action('FrontController@resources') }}">Récolte de bananes</a></li>
             <li><a href="#">Terrain</a></li>
             <li><a href="{{ action('FrontController@map') }}">Jungle</a></li>
             <li><a href="#">Aide</a></li>
